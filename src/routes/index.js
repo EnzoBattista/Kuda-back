f5af22b6b3ce524157497deb547245d5742e75e0
alvaroauth <alvarofranciscoaufmuth@gmail.com>
2026-05-01 13:07:28 -0300

    Add: Agrego los endpoints de usuario

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const sociosRouter = require("./socios.routes");
 const planesRouter = require("./planes.routes");
 const pagosRouter = require("./pagos.routes");
+const usuariosRouter = require("./usuarios.routes");
 
 router.get("/", (req, res) => {
   res.json({
@@ -12,6 +13,7 @@ router.get("/", (req, res) => {
       socios: "/api/socios",
       planes: "/api/planes",
       pagos: "/api/pagos",
+      usuarios: "/api/usuarios",
     },
   });
 });
@@ -19,5 +21,6 @@ router.get("/", (req, res) => {
 router.use("/api/socios", sociosRouter);
 router.use("/api/planes", planesRouter);
 router.use("/api/pagos", pagosRouter);
+router.use("/api/usuarios", usuariosRouter);
 
 module.exports = router;
