0afc8a19061b9a479017e537b7b41bd738ac6706
alvaroauth <alvarofranciscoaufmuth@gmail.com>
2026-05-27 22:18:56 -0300

    .

--- src/routes/usuarios/recepcionistas.routes.js
+++ src/routes/usuarios/recepcionistas.routes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const auth = require("../../middleware/auth.middleware");
 const requirePermiso = require("../../middleware/requirePermiso");
 const { PERMISOS } = require("../../constants/permisos");
-const { createRecepcionista } = require("../../controllers/usuarios/recepcionistas.controller");
+const { createRecepcionista, deleteRecepcionista } = require("../../controllers/usuarios/recepcionistas.controller");
 
 const router = express.Router();
 
 router.post("/", auth, requirePermiso(PERMISOS.ADMINISTRATIVO_GESTIONAR), createRecepcionista);
+router.delete("/:email", auth, requirePermiso(PERMISOS.ADMINISTRATIVO_GESTIONAR), deleteRecepcionista);
 
 module.exports = router;
